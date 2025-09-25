c79368afc883ce4de3e0fe41f71d906a22118b59
AkumaNoyona <pikilm007@gmail.com>
2025-09-26 00:50:45 +0300

    Tasks

--- PR3/src/ru/mirea/uibo1/myasnikova/pr3/TestMove.java
+++ PR3/src/ru/mirea/uibo1/myasnikova/pr3/TestMove.java
@@ -2,18 +2,17 @@ package ru.mirea.uibo1.myasnikova.pr3;
 
 public class TestMove {
     public static void main(String[] args) {
-        // === Задание 1: Shape ===
-        Shape s1 = new Circle(5.5, "RED", false);  // Upcast
+
+        Shape s1 = new Circle(5.5, "RED", false);
         System.out.println(s1);
         System.out.println("Area: " + s1.getArea());
         System.out.println("Perimeter: " + s1.getPerimeter());
         System.out.println("Color: " + s1.getColor());
 
-        Circle c1 = (Circle) s1; // Downcast
+        Circle c1 = (Circle) s1;
         System.out.println(c1);
         System.out.println("Radius: " + c1.getRadius());
 
-        // Ошибка: Shape s2 = new Shape();  // Нельзя создать объект абстрактного класса
 
         Shape s3 = new Rectangle(2.0, 3.0, "Blue", true);
         System.out.println(s3);
@@ -31,7 +30,6 @@ public class TestMove {
         System.out.println(sq1);
         System.out.println("Side: " + sq1.getSide());
 
-        // === Задание 2: Movable ===
         Movable m1 = new MovablePoint(0, 0, 2, 2);
         System.out.println(m1);
         m1.moveUp();
