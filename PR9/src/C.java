b0b4b9cb19f423ba2670279727ea78028509ad0f
AkumaNoyona <pikilm007@gmail.com>
2025-11-07 09:35:42 +0300

    Tasks

---
+++ PR9/src/C.java
@@ -0,0 +1,10 @@
+public class C {
+
+    public static void main(String[] args) {
+        C c = new C();
+        c.method1();
+        method2();
+    }
+    public void method1() {}
+    public static void method2() {}
+}
---
+++ PR9/src/Task1.java
@@ -0,0 +1,17 @@
+public class Task1 {
+    int count;
+    public static void main(String[] args) {
+        // ...
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++)
+            result *= i;
+        return result;
+    }
+}
